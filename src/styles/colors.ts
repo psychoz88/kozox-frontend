48d7b5339d6c1b68e976b73663b87241b61f6642
Volodymyr Zam <vzamotailo@brightgrove.com>
2023-09-22 14:51:03 +0300

    some colors added

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,12 +1,17 @@
-export const mainColor = '#0a1012';
+// Main colors
+export const mainColor = '#060f11';
 export const blackColor = '#000000';
 export const whiteColor = '#ffffff';
 export const successColor = '#52c41a';
 export const warnColor = '#fcb400';
 export const infoColor = '#005e73';
 export const errorColor = '#f5222d';
-export const secondaryColor = '#8c8c8c';
 export const disabledColor = 'rgba(0, 0, 0, 0.25)';
+
+// Text colors
+export const lightGreenTextColor = '#00d993'; // green as code
+export const greyTextColor = '#e4e4e6'; // on nav
+export const dirtyWhiteTextColor = '#e7e7e8'; // use for white textes
 
 // Components like Typography or Icon handles colors from object below
 // so to add color to such components you just need to add color here
@@ -14,11 +19,11 @@ export const app_colors = {
   main: mainColor,
   black: blackColor,
   white: whiteColor,
-  secondary: secondaryColor,
   disabled: disabledColor,
   error: errorColor,
   success: successColor,
   warn: warnColor,
   info: infoColor,
   transparent: 'inherit',
+  lightGreenTextColor: lightGreenTextColor,
 };
