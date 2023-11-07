a6c0d52b6999442fceb45a4e50c267d3455f91c8
Volodymyr Zam <vzamotailo@brightgrove.com>
2023-11-07 12:45:16 +0200

    solved sending to chats

--- src/constants/telegramBot.tsx
+++ src/constants/telegramBot.tsx
@@ -1,2 +1,3 @@
 export const BOT_TOKEN = '6354779574:AAG2mRG9tIzXG3qv2oHHFcgDA-9521qqxvM';
 export const CHAT_ID = '190781466';
+export const CHAT_ID_J = '102274066';
--- src/infrastructure/repository/AppRepository.ts
+++ src/infrastructure/repository/AppRepository.ts
@@ -1,11 +1,15 @@
-import {BOT_TOKEN, CHAT_ID} from 'constants/telegramBot';
+import {BOT_TOKEN, CHAT_ID, CHAT_ID_J} from 'constants/telegramBot';
 import {ApiType, IApi} from 'infrastructure/common/apiService';
+
+const chatIds = [CHAT_ID, CHAT_ID_J]; // max 30 messages per second in Telegram
 
 const appRepository = (api: IApi) => ({
   sentContactFormToTelegramBot: async (data: any) =>
-    await api.get(
-      ApiType.Telegram,
-      `bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=Name%3A+${data.name}%2E+Email%3A+${data.email}%2E+Note%3A+${data.note}%2E`,
+    await chatIds.map((id) =>
+      api.get(
+        ApiType.Telegram,
+        `bot${BOT_TOKEN}/sendMessage?chat_id=${id}&text=Name%3A+${data.name}%2E+Email%3A+${data.email}%2E+Note%3A+${data.note}%2E`,
+      ),
     ),
 });
 
