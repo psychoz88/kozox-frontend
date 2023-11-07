fa48237b5d431000efa7108cd3e0df8be3c4a4e1
volodymyr.zamotailo <volodymyr.zamotailo@ionidea.com>
2023-11-07 12:12:01 +0200

    some improves

--- src/store/app/actions.ts
+++ src/store/app/actions.ts
@@ -8,10 +8,11 @@ export const getContactFormData =
   (dispatch: AppDispatch, _: any, {appContainer}: TContainer) => {
     appContainer.sentContactFormToTelegramBot({
       payload: formData,
-      onSuccess: () => {
+      onSuccess: (data: any) => {
         // (data: any)
         // if need response - use (data: any) instead of formData
-        dispatch(setContactFormData(formData));
+        // if need data from input - use formData instead of data
+        dispatch(setContactFormData(data));
       },
       onError: (error: any) => {
         console.log(error, 'error');
