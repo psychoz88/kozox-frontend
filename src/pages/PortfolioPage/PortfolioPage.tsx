3cbb08be7ea61e12e6e5bfd1e9b0f24565021ce4
volodymyr.zamotailo <volodymyr.zamotailo@ionidea.com>
2023-11-24 12:43:05 +0200

    added content on Profile

--- src/pages/PortfolioPage/PortfolioPage.tsx
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import QuadrupleBlock from 'components/Quadruple/QuadrupleBlock';
 
-import mainImage from 'images/portfolio/nwMainPage.png';
+import nwMainPage from 'images/portfolio/nwMainPage.png';
 import nwVenueProfile from 'images/portfolio/nwVenueProfile.png';
 import nwOfferCreation from 'images/portfolio/nwOfferCreation.png';
+import avtorPartners from 'images/portfolio/avtorParters.png';
+import avtorDownloads from 'images/portfolio/avtorDownloads.png';
+import avtorSolution from 'images/portfolio/avtorSolutions.png';
+import pointerMain from 'images/portfolio/pointerMain.png';
+import pointerMulti from 'images/portfolio/pointerMulti.png';
+import pointerSurvey from 'images/portfolio/pointerSurvey.png';
 
 import {Devider} from 'components/Devider/Devider.styles';
 
@@ -25,29 +31,45 @@ const PortfolioPage = () => {
             well catered for.
           </div>
         }
-        thirdBlock={mainImage}
+        thirdBlock={nwMainPage}
         foursBlock={[nwVenueProfile, nwOfferCreation]}
         isShowed
       />
       <Devider />
       <QuadrupleBlock
-        title='HRS: NewWork'
-        websiteLink='https://newwork.hrs.com/'
+        title='Avtor'
+        websiteLink='https://avtor.ua/'
         secondBlock={
           <div>
-            Our meeting room providers are there to help you and your team to
-            get the business done whichever way suits best your team and your
-            task. NewWork helps you to book a meeting room space within the
-            venues where you already have a work desk booked or venture out to
-            one of our hotel providers who can cater for larger get togethers
-            with plentiful of extras to choose from making sure your team is
-            well catered for.
+            Limited Liability Company "AVTOR" is one of the leading developers
+            of products and solutions in the industry of cryptographic
+            information protection, including the use of electronic digital
+            signature. The company's products have positive expert conclusions
+            of the State Service of Special Communications and Information
+            Protection of Ukraine.
           </div>
         }
-        thirdBlock={mainImage}
-        foursBlock={[nwVenueProfile, nwOfferCreation]}
+        thirdBlock={avtorPartners}
+        foursBlock={[avtorDownloads, avtorSolution]}
         isShowed
         isReverse
+      />
+      <Devider />
+      <QuadrupleBlock
+        title='PointerPro'
+        websiteLink='https://pointerpro.com/'
+        secondBlock={
+          <div>
+            Pointerpro is an all-in-one platform to create attractive online
+            assessments and automatically deliver personalized advice reports in
+            PDF. Trusted by more than 1.000 companies like consultants, coaches,
+            HR and marketers, to build maturity assessments, personality tests,
+            risk assessments, guided selling assessments, and much more.
+          </div>
+        }
+        thirdBlock={pointerMain}
+        foursBlock={[pointerMulti, pointerSurvey]}
+        isShowed
       />
     </div>
   );
