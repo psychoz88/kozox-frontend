1778f1f1100e976687a481b14717bef438daa7eb
Volodymyr Zam <vzamotailo@brightgrove.com>
2023-12-08 07:44:50 +0200

    some stuff

--- src/pages/MainLandingPage/MainLanding.tsx
+++ src/pages/MainLandingPage/MainLanding.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import TwoColumnsBlock from 'components/TwoColumnsBlock';
 import CageBlock from 'components/CageBlock/CageBlock';
 
 import {dataWhyChooseUS} from './helper';
 
-import {Devider} from 'components/Devider/Devider.styles';
+// import {Devider} from 'components/Devider/Devider.styles';
 
 const MainLanding = () => {
   const additionalText = `Indeed, many of these values can become pathological when taken to an
@@ -14,15 +13,7 @@ const MainLanding = () => {
 
   return (
     <div>
-      <TwoColumnsBlock
-        title='First info block'
-        isShowed
-        colLeft={<div>Left side</div>}
-        colRight={<div>Right side</div>}
-        footer={<div>Footer block</div>}
-        customPadding='0'
-      />
-      <Devider />
+      {/* <Devider /> */}
       <CageBlock
         data={dataWhyChooseUS}
         title='Why choose us'
